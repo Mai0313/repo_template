5627111b41f79f0668525b13f3c19902820413c2
wei <mai@mai0313.com>
2025-10-19 17:20:42 +0800

    refactor: update package version retrieval to use dynamic package name

--- src/repo_template/__init__.py
+++ src/repo_template/__init__.py
@@ -1,3 +1,6 @@
 from importlib.metadata import version
+from pathlib import Path
 
-__version__ = version("repo_template")
+package_name = Path(__file__).parent.name
+__package__ = package_name
+__version__ = version(package_name)
